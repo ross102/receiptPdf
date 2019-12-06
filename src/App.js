41a7da51d13818e609143f4ecad1062d140aa700
unknown <nduross1@gmail.com>
2019-12-06 02:36:55 -0800

    finished primary steps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import axiox from 'axios';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Api from './Api';
+import { saveAs } from 'file-saver';
+
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    <h2>Hello there</h2>
-          </div>
-  );
+class App extends Component {
+  state = {
+    name: '',
+    receiptId: 0,
+    price1: 0,
+    price2: 0,
+  }
+
+  handleChange = ({ target: { value, name }}) => this.setState({ [name]: value })
+
+  
+  createAndDownloadPdf = () => {
+
+    Api.post('/create-pdf', this.state)
+      .then(() => Api.get('fetch-pdf', { responseType: 'blob' }))
+      .then((res) => {
+        const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
+
+        saveAs(pdfBlob, 'newPdf.pdf');
+      })
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="text" placeholder="Name" name="name" onChange={this.handleChange}/>
+        <input type="number" placeholder="Receipt ID" name="receiptId" onChange={this.handleChange} />
+        <input type="number" placeholder="Price 1" name="price1" onChange={this.handleChange} />
+        <input type="number" placeholder="Price 2" name="price2" onChange={this.handleChange} />
+        <button onClick={this.createAndDownloadPdf}>Download PDF</button>
+      </div>
+    );
+  }
 }
 
 export default App;
